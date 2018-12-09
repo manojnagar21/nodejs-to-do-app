57643b63ee07a8f4795ec2ba008be00ead39b38e
Mritunjay .N .Nagar <manojnagar21@gmail.com>
2018-12-09 17:45:34 +0530

    input with yargs

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
 const lodash = require('lodash');
+const yargs = require('yargs');
 console.log(lodash.isString(true));
 console.log(lodash.isString('abcdefgh'));
 var filteredarray = lodash.uniq(['abcdefgh', 'a', 'abcdefgh']);
 
 console.log(filteredarray);
-var res = notes.addnote();
-var resadd = notes.add(14, -4);
-console.log(resadd);
-console.log(res);
+//var res = notes.addnote();
+//var resadd = notes.add(14, -4);
+//console.log(resadd);
+//console.log(res);
 console.log(os.userInfo());
 var user = os.userInfo();
 fs.appendFile('greetings.txt', `Hello ${user.username} ${notes.msg}`, function(err){
@@ -26,3 +27,37 @@ fs.appendFile('greetings.txt', `Hello ${user.username} ${notes.msg}`, function(e
         console.log('unable to write the file');
     }
 });
+
+
+
+const argv = yargs.argv;
+console.log(process.argv);
+var command = process.argv[2];
+var command = argv._[0];
+console.log('Command', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+if(command === 'add')
+{
+    console.log('Adding new note');
+    notes.addnote(argv.title, argv.body);
+}
+else if(command === 'list')
+{
+    console.log('Listing all notes');
+    notes.getall();
+}
+else if(command === 'read')
+{
+    console.log('Reading notes');
+    notes.getnote(argv.title);
+}
+else if(command === 'remove')
+{
+    console.log('Removing notes');
+    notes.removenote(argv.title);
+}
+else
+{
+    console.log('Command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,10 +1,32 @@
 console.log('starting notes.js');
 console.log(module);
 module.exports.msg = 'hello';
-module.exports.addnote = () => {
-    console.log('addnote');
-    return 'new note';
-};
+// module.exports.addnote = () => {
+//     console.log('addnote');
+//     return 'new note';
+// };
 module.exports.add = (a, b) => {
     return a + b;
-}
+};
+var addnote = (title, body) => {
+    console.log('Adding note', title, body);
+
+};
+var getall = () => {
+    console.log('Getting all notes');
+
+};
+var getnote = (title) => {
+    console.log('Getting note', title);
+
+};
+var removenote = (title) => {
+    console.log('Removing note', title);
+
+};
+module.exports = {
+    addnote,
+    getall,
+    getnote,
+    removenote
+};
