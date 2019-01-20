6a4b37441e95fdc3caeada1dbcf5b0d1a8f3ac24
Mritunjay .N .Nagar <manojnagar21@gmail.com>
2019-01-20 19:26:32 +0530

    lecture 16

--- notes.js
+++ notes.js
@@ -1,5 +1,6 @@
 console.log('starting notes.js');
 console.log(module);
+const fs = require('fs');
 module.exports.msg = 'hello';
 // module.exports.addnote = () => {
 //     console.log('addnote');
@@ -11,6 +12,27 @@ module.exports.add = (a, b) => {
 var addnote = (title, body) => {
     console.log('Adding note', title, body);
 
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+    try
+    {
+        var notesstring = fs.readFileSync('notes-data.json');
+        //console.log('abcdefgh-note' + notesstring);
+        notes = JSON.parse(notesstring);
+    }
+    catch(e)
+    {
+
+    }
+    var duplicatenotes = notes.filter((note) => note.title == title);
+    if(duplicatenotes.length === 0)
+    {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes)); 
+    }
 };
 var getall = () => {
     console.log('Getting all notes');
