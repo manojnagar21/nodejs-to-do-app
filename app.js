ee8bd2207acf7ad44c989306bbbcf593e8ea6b10
Mritunjay .N .Nagar <manojnagar21@gmail.com>
2019-01-20 23:51:23 +0530

    lecture 17

--- app.js
+++ app.js
@@ -40,7 +40,18 @@ console.log('Yargs', argv);
 if(command === 'add')
 {
     console.log('Adding new note');
-    notes.addnote(argv.title, argv.body);
+    var note = notes.addnote(argv.title, argv.body);
+    if(note)
+    {
+        console.log('Note created');
+        console.log('-----');
+        console.log(`Title ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    }
+    else
+    {
+        console.log('Note title taken');
+    }
 }
 else if(command === 'list')
 {
--- notes.js
+++ notes.js
@@ -9,29 +9,38 @@ module.exports.msg = 'hello';
 module.exports.add = (a, b) => {
     return a + b;
 };
-var addnote = (title, body) => {
-    console.log('Adding note', title, body);
-
-    var notes = [];
-    var note = {
-        title,
-        body
-    };
+var fetchnotes = () => {
+    
     try
     {
         var notesstring = fs.readFileSync('notes-data.json');
         //console.log('abcdefgh-note' + notesstring);
-        notes = JSON.parse(notesstring);
+        //notes = JSON.parse(notesstring);
+        return JSON.parse(notesstring);
     }
     catch(e)
     {
-
+        return [];
     }
+};
+var savenotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes)); 
+};
+var addnote = (title, body) => {
+    console.log('Adding note', title, body);
+
+    //var notes = [];
+    var notes = fetchnotes();
+    var note = {
+        title,
+        body
+    };
     var duplicatenotes = notes.filter((note) => note.title == title);
     if(duplicatenotes.length === 0)
     {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes)); 
+        savenotes(notes);
+        return note;
     }
 };
 var getall = () => {
